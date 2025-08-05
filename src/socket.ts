e2578535701ae510fbecb3b77a1421a1d45c2d6b
Vedant_Jathar <jatharvedant16@gmail.com>
2025-08-05 10:03:19 +0530

    set up the web socket server

---
+++ src/socket.ts
@@ -0,0 +1,21 @@
+import { createServer } from "node:http";
+import { Server } from "socket.io"
+
+const ws_server = createServer()
+
+const io = new Server(ws_server, { cors: { origin: "http://localhost:5173" } })
+
+io.on("connection", (socket) => {
+    console.log("Client connected", socket.id);
+
+    socket.on("join", (data) => {
+        socket.join(String(data.tenantId))
+        socket.emit("join", { roomId: String(data.tenantId) })
+    })
+
+})
+
+export default {
+    ws_server,
+    io
+}
